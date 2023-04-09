54088ffa3a2298c54b0d1dcc8c8eeb43917a1921
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-04-08 22:58:58 -0300

    feat: added custom options

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -1,3 +1,17 @@
+local opt = vim.opt
+local g = vim.g
+local config = require("core.utils").load_config()
+
+
+opt.relativenumber = true
+opt.wrap = false
+opt.textwidth = 0
+opt.scrolloff = 10
+opt.sidescrolloff = 10
+opt.spell = true
+opt.spelllang = { "en_us" }
+opt.colorcolumn = "80"
+
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
