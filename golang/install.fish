9ae30329d7e7467ab2cbfad12d0d2f40d1b60abf
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-07-05 08:41:56 -0300

    feat: added goreman as a tool for my developer workflow

--- golang/install.fish
+++ golang/install.fish
@@ -4,6 +4,7 @@ fish_add_path -a $GOPATH/bin /usr/local/go/bin
 
 if command -qs go
     go install github.com/go-delve/delve/cmd/dlv@latest
+    go install github.com/mattn/goreman@latest
 end
 
 alias --save gmt='go mod tidy'
