087b2d66a92fbb4d813d2a672d564ad515ff2798
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-07-05 08:41:01 -0300

    fix: change PROJECTS path to the path that I use

--- 00-dotfiles/install.fish
+++ 00-dotfiles/install.fish
@@ -3,20 +3,20 @@ set -Ux EDITOR nvim
 set -Ux VISUAL $EDITOR
 
 set -Ux DOTFILES ~/.dotfiles
-set -Ux PROJECTS ~/Developer
+set -Ux PROJECTS ~/dev
 
 fish_add_path -a $DOTFILES/bin $HOME/.bin $HOME/.local/bin
 
 for f in $DOTFILES/*/functions
-	if not contains $f $fish_function_path
-		set -Up fish_function_path $f
-	end
+    if not contains $f $fish_function_path
+        set -Up fish_function_path $f
+    end
 end
 
 for f in $DOTFILES/*/conf.d/*.fish
-	ln -sf $f $__fish_config_dir/conf.d/(basename $f)
+    ln -sf $f $__fish_config_dir/conf.d/(basename $f)
 end
 
 if test -f ~/.localrc.fish
-	ln -sf ~/.localrc.fish $__fish_config_dir/conf.d/localrc.fish
+    ln -sf ~/.localrc.fish $__fish_config_dir/conf.d/localrc.fish
 end
