a9a5ffb561cfeee1e7e2a4e242004ddd381928f7
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-07-05 22:46:33 -0300

    feat: added auto-dark-mode

--- nvim/config/lua/plugins/lazy.lua
+++ nvim/config/lua/plugins/lazy.lua
@@ -207,4 +207,19 @@ return {
   { "tpope/vim-eunuch", event = "BufEnter" },
   { "tpope/vim-sleuth", event = "BufEnter" },
   { "tpope/vim-speeddating", event = "BufEnter" },
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option("background", "dark")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option("background", "light")
+      end,
+    },
+    init = function()
+      require("auto-dark-mode").init()
+    end,
+  },
 }
