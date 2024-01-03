7e9bca9848fce1dfef1d8c27a92863171d9122e7
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-01-03 17:54:32 -0300

    refactor[fish]: just use a simple function intead of a plugin that doesn't work

---
+++ system/functions/env-source.fish
@@ -0,0 +1,8 @@
+function env-source -d "load env file for the current shell"
+    for i in (cat $argv)
+        set arr (echo $i |tr = \n)
+        if not test -z $arr[1]
+            set -gx $arr[1] $arr[2]
+        end
+    end
+end
