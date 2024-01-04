591894583586d1f406945b0a6a6a6d29608e24bd
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-01-04 10:34:24 -0300

    feat[fish]: make env-source ignore commented env lines

--- system/functions/env-source.fish
+++ system/functions/env-source.fish
@@ -1,7 +1,7 @@
 function env-source -d "load env file for the current shell"
     for i in (cat $argv)
         set arr (echo $i |tr = \n)
-        if not test -z $arr[1]
+        if not test -z $arr[1] && string match -q -r "^[a-zA-Z_-]+" $arr[1]
             set -gx $arr[1] $arr[2]
         end
     end
