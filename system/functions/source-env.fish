1fb512e6454e09ed548119acc4a5bca8f1ce1198
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-12-26 11:55:50 -0300

    refactor: better function to load envs

--- system/functions/source-env.fish
+++
@@ -1,8 +0,0 @@
-function source-env -d "load env file for the current shell"
-    for i in (cat $argv)
-        set arr (echo $i |tr = \n)
-        if not test -z $arr[1] && string match -q -r "^[a-zA-Z_-]+" $arr[1]
-            set -gx $arr[1] $arr[2]
-        end
-    end
-end
