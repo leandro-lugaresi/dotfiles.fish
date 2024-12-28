78544af5d9b3c6fc281257b373c4534703310f01
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-12-28 14:28:15 -0300

    refactor: add description for some keymaps

--- nvim/config/lua/user/keymaps.lua
+++ nvim/config/lua/user/keymaps.lua
@@ -1,25 +1,25 @@
-local opts = { noremap = true, silent = true }
-
 -- Shorten function name
-local keymap = vim.keymap.set
+local keymap = function(mode, keys, func, desc)
+  vim.keymap.set(mode, keys, func, { desc = desc, noremap = true, silent = true })
+end
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>", "Leader key")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
 -- disable Ex mode, I always enter in it by mistake
-keymap("n", "Q", "<Nop>", opts)
+keymap("n", "Q", "<Nop>", "avoid quiting vim")
 
 -- create and edit new buffer
-keymap("n", "<leader>n", ":enew<CR>", opts)
+keymap("n", "<leader>fn", ":enew<CR>", "New buffer")
 
 -- quicklists
-keymap("n", "<leader>co", ":copen<CR>", opts)
-keymap("n", "<leader>cc", ":cclose<CR>", opts)
-keymap("n", "[q", ":cprevious<CR>zz", opts)
-keymap("n", "]q", ":cnext<CR>zz", opts)
+keymap("n", "<leader>co", ":copen<CR>")
+keymap("n", "<leader>cc", ":cclose<CR>")
+keymap("n", "[q", ":cprevious<CR>zz")
+keymap("n", "]q", ":cnext<CR>zz")
 
 -- Better window navigation
 -- keymap("n", "<C-h>", "<C-w>h", opts)
@@ -34,75 +34,74 @@ keymap("n", "]q", ":cnext<CR>zz", opts)
 -- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- buffer nav
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", "Next buffer")
+keymap("n", "<S-h>", ":bprevious<CR>", "Previous buffer")
 
 -- save and quit
-keymap("n", "<leader>w", ":write<CR>", opts)
-keymap("n", "<leader>s", ":write<CR>", opts)
+keymap("n", "<leader>w", ":write<CR>", "Save file")
 
 -- paste over without replacing default register
-keymap("n", "<leader>p", '"_dP', opts)
+keymap("n", "<leader>p", '"_dP', "Paste without replacing register")
 
 -- keep more or less in the same place when going next
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
+keymap("n", "n", "nzzzv", "")
+keymap("n", "N", "Nzzzv", "")
 
 -- keep more or less in the same place when going up/down
-keymap("n", "<C-u>", "<C-u>zz", opts)
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-o>", "<C-o>zz", opts)
-keymap("n", "<C-i>", "<C-i>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", "")
+keymap("n", "<C-d>", "<C-d>zz", "")
+keymap("n", "<C-o>", "<C-o>zz", "")
+keymap("n", "<C-i>", "<C-i>zz", "")
 
 -- move record macro to Q instead of q
-keymap("n", "Q", "q", opts)
-keymap("n", "q", "<Nop>", opts)
+keymap("n", "Q", "q", "Record macro")
+keymap("n", "q", "<Nop>", "")
 
 -- Insert empty blank line above/bellow
-keymap("n", "]<Space>", "m`o<Esc>``", opts)
-keymap("n", "[<Space>", "m`O<Esc>``", opts)
+keymap("n", "]<Space>", "m`o<Esc>``")
+keymap("n", "[<Space>", "m`O<Esc>``")
 
 -- system clipboard integration
-keymap("n", "<leader>y", '"+y', opts)
-keymap("n", "<leader>Y", '"+Y', opts)
+keymap("n", "<leader>y", '"+y', "Copy to clipboard")
+keymap("n", "<leader>Y", '"+Y', "Copy to clipboard")
 
 -- delete to blackhole
-keymap("n", "<leader>d", '"_d', opts)
-keymap("n", "<leader>D", '"_D', opts)
+keymap("n", "<leader>d", '"_d')
+keymap("n", "<leader>D", '"_D')
 
 -- Insert --
 -- in insert mode, adds new undo points after , . ! and ?.
-keymap("i", "-", "-<c-g>u", opts)
-keymap("i", "_", "_<c-g>u", opts)
-keymap("i", ",", ",<c-g>u", opts)
-keymap("i", ".", ".<c-g>u", opts)
-keymap("i", "!", "!<c-g>u", opts)
-keymap("i", "?", "?<c-g>u", opts)
+keymap("i", "-", "-<c-g>u")
+keymap("i", "_", "_<c-g>u")
+keymap("i", ",", ",<c-g>u")
+keymap("i", ".", ".<c-g>u")
+keymap("i", "!", "!<c-g>u")
+keymap("i", "?", "?<c-g>u")
 
 -- alias quick jk to esc
-keymap("i", "jk", "<ESC>", opts)
+keymap("i", "jk", "<ESC>")
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap("v", "<A-j>", ":m .+1<CR>==")
+keymap("v", "<A-k>", ":m .-2<CR>==")
+keymap("v", "p", '"_dP')
 
 -- system clipboard integration
-keymap("v", "<leader>y", '"+y', opts)
-keymap("v", "<leader>Y", '"+Y', opts)
+keymap("v", "<leader>y", '"+y')
+keymap("v", "<leader>Y", '"+Y')
 
 -- delete to blackhole
-keymap("v", "<leader>d", '"_d', opts)
-keymap("v", "<leader>D", '"_D', opts)
+keymap("v", "<leader>d", '"_d')
+keymap("v", "<leader>D", '"_D')
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv")
+keymap("x", "K", ":move '<-2<CR>gv-gv")
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv")
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv")
