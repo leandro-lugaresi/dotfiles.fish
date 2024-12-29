0d80b73bf81d54e6348fd4eda69fed39f0de4a29
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-12-28 21:36:12 -0300

    fix: add correct config setup for tms

--- tmux/install.fish
+++ tmux/install.fish
@@ -6,7 +6,7 @@ end
 test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 if command -qs tms
-    tms config --full-path true --paths $PROJECTS $DOTFILES
+    tms config --full-path false --paths $PROJECTS $DOTFILES --search-submodules false --max-depths 5 --picker-highlight-color="#181825" --picker-highlight-text-color="#ffffff"
 end
 
 alias --save ta='tmux-new'
