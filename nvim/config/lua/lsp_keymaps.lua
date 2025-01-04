fdaeb2138d8dd791a9d93c4bcfccdbe706091f83
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2025-01-04 14:46:26 -0300

    refactor(nvim): improve lsp keymaps

--- nvim/config/lua/lsp_keymaps.lua
+++ nvim/config/lua/lsp_keymaps.lua
@@ -1,43 +1,51 @@
 local M = {}
 
---- Add a normal keymap.
----@param lhs string Keymap
----@param rhs function Action
----@param bufnr number Buffer number
-local keymap = function(lhs, rhs, bufnr)
-  vim.keymap.set("n", lhs, rhs, {
-    noremap = true,
-    silent = true,
-    buffer = bufnr,
-  })
-end
-
 --- On attach for key maps.
 ---@param bufnr number Buffer number
 M.on_attach = function(bufnr)
   local builtin = require("telescope.builtin")
-  keymap("gd", builtin.lsp_definitions, bufnr)
-  keymap("gr", builtin.lsp_references, bufnr)
-  keymap("<C-j>", builtin.lsp_document_symbols, bufnr)
-  keymap("<C-h>", builtin.lsp_dynamic_workspace_symbols, bufnr)
-  keymap("<C-k>", vim.lsp.buf.signature_help, bufnr)
-  keymap("gi", builtin.lsp_implementations, bufnr)
-  keymap("gD", vim.lsp.buf.declaration, bufnr)
-  keymap("K", vim.lsp.buf.hover, bufnr)
-  keymap("<leader>D", builtin.lsp_type_definitions, bufnr)
-  keymap("<leader>cl", vim.lsp.codelens.run, bufnr)
-  keymap("<leader>rn", vim.lsp.buf.rename, bufnr)
-  keymap("<leader>ca", vim.lsp.buf.code_action, bufnr)
-  keymap("<leader>tn", vim.diagnostic.open_float, bufnr)
-  keymap("<leader>lr", vim.cmd.LspRestart, bufnr)
+  local wk = require("which-key")
+
+  --- Add a normal keymap.
+  ---@param keys string Keymap
+  ---@param func function Action
+  ---@param desc string  Description
+  local keymap = function(keys, func, desc)
+    vim.keymap.set("n", keys, func, {
+      noremap = true,
+      silent = true,
+      buffer = bufnr,
+      desc = "LSP: " .. desc,
+    })
+  end
+
+  keymap("gd", builtin.lsp_definitions, "Goto Definition")
+  keymap("gr", builtin.lsp_references, "Goto References")
+  keymap("<C-j>", builtin.lsp_document_symbols, "Document Symbols")
+  keymap("<C-h>", builtin.lsp_dynamic_workspace_symbols, "Workspace Symbols")
+  keymap("<C-k>", vim.lsp.buf.signature_help, "Signature Help")
+  keymap("gi", builtin.lsp_implementations, "Goto Implementation")
+  keymap("gD", vim.lsp.buf.declaration, "Goto Declaration")
+  keymap("K", vim.lsp.buf.hover, "Hover Documentation")
   keymap("[d", function()
     vim.diagnostic.goto_prev({ float = false })
     vim.cmd("norm zz")
-  end, bufnr)
+  end, "Next Diagnostic")
   keymap("]d", function()
     vim.diagnostic.goto_next({ float = false })
     vim.cmd("norm zz")
-  end, bufnr)
+  end, "Previous Diagnostic")
+
+  wk.add({
+    { "<leader>la", vim.lsp.buf.code_action, desc = "Code Action" },
+    { "<leader>lA", vim.lsp.buf.range_code_action, desc = "Range Code Actions" },
+    { "<leader>ls", vim.lsp.buf.signature_help, desc = "Display Signature Information" },
+    { "<leader>rn", vim.lsp.buf.rename, desc = "Rename all references" },
+    { "<leader>lr", vim.cmd.LspRestart, desc = "Rename all references" },
+    { "<leader>lf", vim.lsp.buf.format, desc = "Format" },
+    { "<leader>li", require("telescope.builtin").lsp_implementations, desc = "Implementation" },
+    { "<leader>lw", require("telescope.builtin").diagnostics, desc = "Diagnostics" },
+  })
 end
 
 return M
